a4fd2b7465fe12fdd69a62922879b83e7068420d
CoCoTea <49379848+CoCoTeaNet@users.noreply.github.com>
2025-12-06 17:46:07 +0800

    复现问题场景

--- src/main/java/com/example/sqltoy_solon_demo/DbConfig.java
+++ src/main/java/com/example/sqltoy_solon_demo/DbConfig.java
@@ -4,6 +4,8 @@ import com.zaxxer.hikari.HikariDataSource;
 import org.noear.solon.annotation.Bean;
 import org.noear.solon.annotation.Configuration;
 import org.noear.solon.annotation.Inject;
+import org.sagacity.sqltoy.dao.LightDao;
+import org.sagacity.sqltoy.solon.annotation.Db;
 
 import javax.sql.DataSource;
 
@@ -12,5 +14,10 @@ public class DbConfig {
     @Bean(name = "db1", typed = true)
     public DataSource db1(@Inject("${myapp.db1}") HikariDataSource ds) {
         return ds;
+    }
+
+    @Bean
+    public void init(@Db LightDao lightDao) {
+        System.out.println("lightDao instance = "+lightDao);
     }
 }
